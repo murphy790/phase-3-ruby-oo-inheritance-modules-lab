60db23dd76645c2a50721f77fc5c7ce7ddeee684
Alex Mwaura <alexmwaura36@gmail.com>
2022-08-22 00:16:40 +0300

    commit tree 60222f86eb911714f1f4c65a271094197c2db17e

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,10 @@
+module Memorable
+
+    def count
+        all.count
+    end
+
+    def reset_all
+        all.clear
+    end
+end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,23 @@
 require 'pry'
 
 class Song
+  extend Memorable
+  extend Findable
+  include Paramable
+
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
-
-  def initialize
+ def initialize
     @@songs << self
   end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -21,10 +26,16 @@ class Song
   def self.reset_all
     self.all.clear
   end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
   def self.count
     self.all.count
   end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
@@ -33,4 +44,7 @@ class Song
   def to_param
     name.downcase.gsub(' ', '-')
   end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
